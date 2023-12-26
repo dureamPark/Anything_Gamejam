269eee77b0a8b3bcf432a2059083955f4103d327
PapaHu22 <144997070+PapaHu22@users.noreply.github.com>
2023-12-26 18:05:28 +0900

    22

--- Anything_GameJam/Assets/scripts/UI/UpgradeWeapon.cs
+++ Anything_GameJam/Assets/scripts/UI/UpgradeWeapon.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class UpgradeWeapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static UpgradeWeapon Instance;
+    public AttackManager AM;
 
-    // Update is called once per frame
-    void Update()
+    public void Madarin_Updrade()
     {
-        
+        Debug.Log("귤 업그레이드");
+        AM.Mandarin_Damage += 1;
+    }
+    public void dekopon_upgrade()
+    {
+        Debug.Log("한라봉 업그레이드");
+        AM.Dekopon_Damage += 1;
+    }
+    public void dolhareubang_upgrade()
+    {
+        Debug.Log("돌하르방 업그레이드");
+        AM.Dolhareubang_damage += 1;
+    }
+    public void sliverfish_upgrade()
+    {
+        Debug.Log("은갈치 업그레이드");
+        AM.Silver_cutlassfish_damage += 1;
     }
 }
--- Anything_GameJam/Assets/scripts/upgradeweapon.cs
+++ Anything_GameJam/Assets/scripts/upgradeweapon.cs
@@ -4,24 +4,5 @@ using UnityEngine;
 
 public class upgradeweapon : MonoBehaviour
 {
-    public void madarin_updrade()
-    {
-        Debug.Log("귤 업그레이드");
-        madarin_damage += 1;
-    }
-    public void dekopon_upgrade()
-    {
-        Debug.Log("한라봉 업그레이드");
-        dekopon_damage += 1;
-    }
-    public void dolhareubang_upgrade()
-    {
-        Debug.Log("돌하르방 업그레이드");
-        dolhareubang_damage += 1;
-    }
-    public void sliverfish_upgrade()
-    {
-        Debug.Log("은갈치 업그레이드");
-        s_damage += 1;
-    }
+   
 }
